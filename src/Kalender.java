5563ac6fd7f393844f63828ec2ea69828c2c9954
daniel.niederh@gmail.com <daniel.niederh@gmail.com>
2020-09-30 14:51:38 +0200

    Funktioniert

--- src/Kalender.java
+++ src/Kalender.java
@@ -1,49 +1,55 @@
 import java.time.temporal.ChronoUnit;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
+import java.util.*;
 import java.time.*;
-import java.util.List;
 
 class Kalender {
+
     public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
         List<LocalDate> Feiertage = new ArrayList<>();
         int jahre = 1;
         int montag = 0, dienstag = 0, mittwoch = 0, donnerstag = 0, freitag = 0, samstag = 0, sonntag = 0;
-        feiertageGenerieren(Feiertage, jahre - 1);
-        System.out.println("Size: " + Feiertage.size());
+        int startjahr, endjahr;
+
+
+        System.out.print("Startjahr: ");
+        startjahr = reader.nextInt();
+
+
+        System.out.print("Endjahr (inklusive): ");
+        endjahr = reader.nextInt();
+        feiertageGenerieren(Feiertage, startjahr, endjahr);
+
         for (int i = 0; i < Feiertage.size(); i++) {
-            System.out.println(Feiertage.get(i).getDayOfWeek());
-            if (Feiertage.get(i).getDayOfWeek().equals("MONDAY")) {
+
+            if (Feiertage.get(i).getDayOfWeek().equals(DayOfWeek.MONDAY)) {
                 montag++;
             }
-            if (Feiertage.get(i).getDayOfWeek().equals("TUESDAY")) {
+            if (Feiertage.get(i).getDayOfWeek().equals(DayOfWeek.TUESDAY)) {
                 dienstag++;
             }
-            if (Feiertage.get(i).getDayOfWeek().equals("WEDNESDAY")) {
+            if (Feiertage.get(i).getDayOfWeek().equals(DayOfWeek.WEDNESDAY)) {
                 mittwoch++;
             }
-            if (Feiertage.get(i).getDayOfWeek().equals("THURSDAY")) {
+            if (Feiertage.get(i).getDayOfWeek().equals(DayOfWeek.THURSDAY)) {
                 donnerstag++;
             }
-            if (Feiertage.get(i).getDayOfWeek().equals("FRIDAY")) {
+            if (Feiertage.get(i).getDayOfWeek().equals(DayOfWeek.FRIDAY)) {
                 freitag++;
             }
-            if (Feiertage.get(i).getDayOfWeek().equals("SATURDAY")) {
+            if (Feiertage.get(i).getDayOfWeek().equals(DayOfWeek.SATURDAY)) {
                 samstag++;
             }
-            if (Feiertage.get(i).getDayOfWeek().equals("SUNDAY")) {
+            if (Feiertage.get(i).getDayOfWeek().equals(DayOfWeek.SUNDAY)) {
                 sonntag++;
-            } else {
-                System.out.println("Falsch");
             }
         }
         feiertagAnzahlAusgeben(montag, dienstag, mittwoch, donnerstag, freitag, samstag, sonntag);
 
     }
 
-    public static void feiertageGenerieren(List<LocalDate> feiertage, int jahre) {
-        for (int i = 2020; i <= 2020 + jahre; i++) {
+    public static void feiertageGenerieren(List<LocalDate> feiertage, int startjahr, int endjahr) {
+        for (int i = startjahr; i <= startjahr + endjahr - startjahr; i++) {
             feiertage.add(LocalDate.of(i, 1, 1));
             feiertage.add(LocalDate.of(i, 1, 6));
             feiertage.add(LocalDate.of(i, 5, 1));
@@ -65,8 +71,3 @@ class Kalender {
         System.out.println("Sonntage: " + so);
     }
 }
-
-
-
-
-
