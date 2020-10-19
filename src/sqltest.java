e70778b1349aa33dab2f13d0c8f0f0a22a5c7f93
daniel.niederh@gmail.com <daniel.niederh@gmail.com>
2020-10-19 11:31:18 +0200

    Sql geat holbats

--- src/sqltest.java
+++ src/sqltest.java
@@ -1,4 +1,5 @@
 import java.sql.*;
+import java.sql.ResultSet;
 public class sqltest {
     public static void main(String[] args) {
 
@@ -24,7 +25,28 @@ public class sqltest {
             System.out.println("* Verbindung aufbauen");
             conn = DriverManager.getConnection("jdbc:mysql://"+hostname+"/"+dbname+"?user="+user+"&password="+password+"&serverTimezone=UTC");
             Statement myStat = conn.createStatement();
-            myStat.execute("INSERT INTO Kalender (Montag, Dienstag, Mittwoch) VALUES (value1, value2, value3)");
+            //myStat.execute("INSERT INTO Kalender values (2)");
+            ResultSet reSe=myStat.executeQuery("Select * from kalender");
+            System.out.println("Montag      Dienstag        Mittwoch        Donnerstag      Freitag     Samstag     Sonntag");
+            while(reSe.next()){
+                String Montag = reSe.getString("Montag");
+                String Dienstag = reSe.getString("Dienstag");
+                String Mittwoch = reSe.getString("Mittwoch");
+                String Donnerstag = reSe.getString("Donnerstag");
+                String Freitag = reSe.getString("Freitag");
+                String Samstag = reSe.getString("Samstag");
+                String Sonntag = reSe.getString("Sonntag");
+
+                System.out.printf("%1s", Montag);
+                System.out.printf("%12s", Dienstag);
+                System.out.printf("%16s", Mittwoch);
+                System.out.printf("%16s", Donnerstag);
+                System.out.printf("%16s", Freitag);
+                System.out.printf("%14s", Samstag);
+                System.out.printf("%14s", Sonntag);
+                System.out.println();
+            }
+
             //resultset für auslesen
             System.out.println("* Datenbank-Verbindung beenden");
             conn.close();
